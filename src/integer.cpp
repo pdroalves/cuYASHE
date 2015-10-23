54bfa1c37931c8cdcd113935aa09a2eecaec1d96
Pedro Alves <pdroalves@gmail.com>
2015-10-23 00:08:20 -0200

    - Added the Integer class. This class should be used to represent integers that frequently are operands of operations with Polynomials.  - Ok for PolyFixture tests.

---
+++ src/integer.cpp
@@ -0,0 +1,144 @@
+#include "integer.h"
+#include "cuda_functions.h"
+
+void Integer::update_device_data(){
+    // uint64_t start = cycles();
+
+    if(this->get_device_updated())
+    	return;
+    else if(!this->get_crt_computed())
+    	this->crt();
+
+    #ifdef VERBOSE
+    std::cout << "Copying data to GPU." << std::endl;
+    #endif
+
+    cudaError_t result;
+    this->ON_COPY = true;
+
+    // if(this->d_polyCRT == NULL){
+      result = cudaMalloc((void**)&this->d_crt_values,(this->crt_values.size())*sizeof(cuyasheint_t));
+      assert(result == cudaSuccess);
+    // }
+
+    result = cudaMemcpy(this->d_crt_values, &crt_values[0] , (this->crt_values.size())*sizeof(cuyasheint_t), cudaMemcpyHostToDevice);
+    assert(result == cudaSuccess);
+
+    result = cudaDeviceSynchronize();
+    assert(result == cudaSuccess);
+
+    this->ON_COPY = false;
+    this->set_device_updated(true);
+}
+
+void Integer::update_host_data(){
+
+    if(this->get_host_updated())
+      return;
+
+    #ifdef VERBOSE
+    std::cout << "Copying data to CPU." << std::endl;
+    #endif
+
+    cudaError_t result;
+
+    assert(Polynomial::CRTPrimes.size() > 0);
+    if(this->crt_values.size() != Polynomial::CRTPrimes.size())
+      this->crt_values.resize(Polynomial::CRTPrimes.size());
+
+    result = cudaMemcpy(&this->crt_values[0],
+                        this->d_crt_values,
+                        Polynomial::CRTPrimes.size()*sizeof(cuyasheint_t), 
+                        cudaMemcpyDeviceToHost);
+    assert(result == cudaSuccess);
+
+    result = cudaDeviceSynchronize();
+    assert(result == cudaSuccess);
+    this->set_host_updated(true);
+    this->icrt();
+}
+
+void Integer::icrt(){
+
+  // Escapes, if possible
+  if(this->get_icrt_computed())
+    return;
+  else if(this->get_host_updated()){
+    this->set_icrt_computed(true);
+    return;
+  } 
+  
+  ZZ M = Polynomial::CRTProduct;
+  std::vector<cuyasheint_t> primes = Polynomial::CRTPrimes;
+  std::vector<ZZ> Mpis = Polynomial::CRTMpi;
+  std::vector<ZZ> invMpis = Polynomial::CRTInvMpi;
+ 
+  // Discards all coefficients and prepare to receive new this->CRTSPACING coefficients
+
+  // Iteration over all primes
+  for(unsigned int i = 0; i < primes.size();i++){
+    // Get a prime 
+    ZZ pi = to_ZZ(primes[i]);
+    ZZ Mpi = Mpis[i];
+    ZZ invMpi = invMpis[i];
+
+    this->value += Mpi*( invMpi*(this->crt_values[i]) % pi);
+  }
+ 
+ 
+  this->set_host_updated(true);
+  this->set_icrt_computed(true);
+  
+  this->value %= M;
+  return;
+}
+
+Polynomial Integer::operator+(Polynomial &a){
+    Polynomial *p;
+    p = new Polynomial();
+    p->copy(a);
+    CUDAFunctions::callPolynomialOPInteger( ADD,
+                                            p->get_stream(),
+                                            p->get_device_crt_residues(),
+                                            this->get_device_crt_residues(),
+                                            p->get_crt_spacing(),
+                                            Polynomial::CRTPrimes.size());
+    return p;
+}
+
+Polynomial Integer::operator*(Polynomial &a){
+    Polynomial *p;
+    p = new Polynomial();
+    p->copy(a);
+    // Apply CRT and copy data to global memory, if needed
+    // #pragma omp parallel sections num_threads(2)
+    {
+        // #pragma omp section
+        {
+          #ifdef VERBOSE
+            std::cout << "p: " << std::endl;
+            #endif
+          if(!p->get_device_updated()){
+            p->update_device_data();
+          }
+
+        }
+        // #pragma omp section
+        {
+          #ifdef VERBOSE
+            std::cout << "b: " << std::endl;
+            #endif
+            if(!this->get_device_updated()){
+            this->update_device_data();
+          }
+        }
+    }
+
+    CUDAFunctions::callPolynomialOPInteger( MUL,
+                                            p->get_stream(),
+                                            p->get_device_crt_residues(),
+                                            this->get_device_crt_residues(),
+                                            p->get_crt_spacing(),
+                                            Polynomial::CRTPrimes.size());
+    return p;
+}
---
+++ src/integer.h
@@ -0,0 +1,227 @@
+#ifndef INTEGER_H
+#define INTEGER_H
+
+#include <NTL/ZZ.h>
+#include "polynomial.h"
+
+class Integer{
+	public:
+		Integer(ZZ a){
+			this->set_value(a);
+
+			this->set_host_updated(true);
+			this->set_device_updated(false);
+			this->set_crt_computed(false);
+			this->set_icrt_computed(true);
+		}
+		Integer(cuyasheint_t a){
+			this->set_value(a);
+
+			this->set_host_updated(true);
+			this->set_device_updated(false);
+			this->set_crt_computed(false);
+			this->set_icrt_computed(true);
+		}
+		Integer operator=(ZZ a){			
+			this->set_value(a);
+
+			this->set_host_updated(true);
+			this->set_device_updated(false);
+			this->set_crt_computed(false);
+			this->set_icrt_computed(true);
+
+			return *this;
+		}
+		Integer operator=(cuyasheint_t a){			
+			this->set_value(a);
+
+			this->set_host_updated(true);
+			this->set_device_updated(false);
+			this->set_crt_computed(false);
+			this->set_icrt_computed(true);
+
+			return *this;
+		}
+		Integer operator/=(Integer a){			
+			if(!this->get_host_updated())
+				this->update_host_data();
+			
+			this->set_device_updated(false);
+			return value / a.get_value();;
+		}
+		ZZ operator+(ZZ a){
+			return value+a;
+		}
+		ZZ operator*(ZZ a){
+			return value*a;
+		}
+		ZZ operator-(ZZ a){
+			return value-a;
+		}
+		ZZ operator-(cuyasheint_t a){
+			return value-a;
+		}
+		ZZ operator/(ZZ a){
+			if(!this->get_host_updated())
+				this->update_host_data();
+			
+			this->set_device_updated(false);
+			value /= a;
+			return value;
+		}
+		ZZ operator%(ZZ a){
+			if(!this->get_host_updated())
+				this->update_host_data();
+			
+			this->set_device_updated(false);
+			value %= a;
+			return value;
+		}
+		Polynomial operator+(Polynomial &a);
+		Polynomial operator*(Polynomial &a);
+		void set_value(ZZ a){
+			value = a;
+
+			this->set_host_updated(true);
+		}
+		void set_value(cuyasheint_t a){
+			value = to_ZZ(a);
+
+			this->set_host_updated(true);
+		}
+		ZZ get_value(){
+			if(!this->get_host_updated())
+				this->update_host_data();
+			return value;
+		}
+		void set_host_updated(bool b){
+	      this->HOST_IS_UPDATED = b;
+	      if(b){        
+	        #ifdef VERBOSE
+	        std::cout << "Host data is updated" << std::endl;
+	        #endif
+	      }else{        
+	        this->set_icrt_computed(false);
+	        #ifdef VERBOSE
+	        std::cout << "Host data is NOT updated" << std::endl;
+	        #endif
+	      }
+	    }
+	    bool get_host_updated(){
+
+	      bool b = this->HOST_IS_UPDATED;
+	      if(b){        
+	        #ifdef VERBOSE
+	        std::cout << "Host data is updated" << std::endl;
+	        #endif
+	      }else{        
+	        this->set_icrt_computed(false);
+	        #ifdef VERBOSE
+	        std::cout << "Host data is NOT updated" << std::endl;
+	        #endif
+	      }
+
+	      return b;
+	    }
+	    void set_device_updated(bool b){
+	      this->DEVICE_IS_UPDATE = b;
+	      if(b){
+	        #ifdef VERBOSE
+	        std::cout << "Device data is updated" << std::endl;
+	        #endif
+	      }
+	      if(!b){
+	        #ifdef VERBOSE
+	        std::cout << "Device data is NOT updated" << std::endl;
+	        #endif
+	        this->set_crt_computed(false);
+	      }
+	    }
+	    bool get_device_updated(){
+	      bool b = this->DEVICE_IS_UPDATE;
+	      if(b){        
+	        #ifdef VERBOSE
+	        std::cout << "Device data is updated" << std::endl;
+	        #endif
+	      }else{        
+	        this->set_icrt_computed(false);
+	        #ifdef VERBOSE
+	        std::cout << "Device data is NOT updated" << std::endl;
+	        #endif
+	      }
+
+	      return b;
+	    }
+	    void set_crt_computed(bool b){
+	      this->CRT_COMPUTED = b;
+	      if(b){        
+	        #ifdef VERBOSE
+	        std::cout << "CRT residues computed" << std::endl;
+	        #endif
+	      }else{        
+	        #ifdef VERBOSE
+	        std::cout << "CRT residues NOT computed" << std::endl;
+	        #endif
+	      }
+	    }
+	    bool get_crt_computed(){
+	      return this->CRT_COMPUTED;
+	    }
+	    void set_icrt_computed(bool b){
+	      this->ICRT_COMPUTED = b;
+
+	      if(b){        
+	        #ifdef VERBOSE
+	        std::cout << "ICRT residues computed" << std::endl;
+	        #endif
+	      }else{        
+	        #ifdef VERBOSE
+	        std::cout << "ICRT residues NOT computed" << std::endl;
+	        #endif
+	      }
+	    }
+	    bool get_icrt_computed(){
+	      return this->ICRT_COMPUTED;
+	    }
+
+		void crt(){
+			// Escapes, if possible
+
+			if(this->get_crt_computed())
+			  return;
+			else if(!this->get_host_updated())
+			  throw "host is not updated";
+
+			// Set the quantity of expected residues
+			std::vector<cuyasheint_t> P = Polynomial::CRTPrimes;
+			this->crt_values.resize(P.size());
+
+			// We pick each prime
+			#pragma omp parallel for  
+			for(unsigned int i = 0; i < P.size();i++)
+		    	crt_values[i] = conv<cuyasheint_t>(value % P[i]);
+			  	
+
+			this->set_device_updated(false);
+			this->set_crt_computed(true);
+		}
+		void icrt();
+		cuyasheint_t* get_device_crt_residues(){
+			return this->d_crt_values;
+		}
+		void update_device_data();
+		void update_host_data();
+	private:
+	    // Attributes
+	    bool ON_COPY;
+	    bool HOST_IS_UPDATED;
+	    bool DEVICE_IS_UPDATE;
+	    bool CRT_COMPUTED;
+	    bool ICRT_COMPUTED;
+
+		ZZ value;
+		std::vector<cuyasheint_t> crt_values;
+		cuyasheint_t *d_crt_values;
+};
+
+#endif
